978396b0ae985b8eaa21f4d876e0268b43e6bed3
kenyounot123 <kenlu519@gmail.com>
2023-11-05 20:52:01 -0500

    Build queries to separate Past and Future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendee_events, foreign_key: 'attended_event_id'
   has_many :attendees, through: :attendee_events
+
+  scope :past, -> { where('date < ?', Date.today).order(date: :desc) } 
+  scope :upcoming, -> { where('date > ?', Date.today).order(:date) }
 end
 
